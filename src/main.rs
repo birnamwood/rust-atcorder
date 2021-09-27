67b66d91d201b2702bff862e27c1e68ab240c45d
birnamwood <birnamwood19@gmail.com>
2021-09-27 13:38:39 +0900

    contest 220

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
 use proconio::input;
 
 fn main() {
-    input!{
-        k: i64,
-        a: i64,
-        b: i64,
+    input! {
+        k: u32,
+        a: String,
+        b: String
     }
-    let ans:i64 = change(k, a) * change(k, b);
-
-    println!("{}", ans);
-}
-
-// x n進数
-fn change(mut a: i64,x: i64)->i64{
-    let mut amari: Vec<i64> = Vec::new();
-    while x != 0{
-        amari.push(x % a);
-        x /= a;
-    }
-    let mut n:i64 = 0;
-    for i in 0..amari.len(){
-         n += 10i64.pow(i as u32)*amari[i]
-    }
-    return n;
+    let a = u64::from_str_radix(&a, k).unwrap();
+    let b = u64::from_str_radix(&b, k).unwrap();
+    println!("{}", a * b);
 }
